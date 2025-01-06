0638e916e6ef3d75af8d186b2b3bdb3a8b09513b
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-01-06 14:02:15 -0500

    docs: add comments explain behavior tested with Hello.tsx
    - That component appears in both server and browser
    - Describe server prerendering and browser hydration process

--- app/components/hello.tsx
+++ app/components/hello.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+// Appeared both in the server and the browser
+// Prerendered in the server, creating a static shell
+// Hydrated in the browser, meaning the JS is executed in the browser
 export default function Hello() {
   console.log("Is this in the browser or server?");
   return <h1>Hello</h1>;
