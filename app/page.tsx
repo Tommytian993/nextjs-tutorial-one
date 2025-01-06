41427e3330833b08574fce43dd9caf7986b7c793
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-01-06 13:49:36 -0500

    feat: import and use Hello component in page to test render behavior

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import Hello from "./components/hello";
 
 export default function Home() {
-  // Appeared in the server, meaning could access server-side resources directly
+  // Appeared in the server, meaning could access server-side resources directly(e.g. database or file system
+  // Thus reducing the amount of JS sent to the client
   console.log("Is this in the browser or server?");
   return (
     <div>
       <h1>Test Next.js</h1>
+      <Hello />
     </div>
   );
 }
