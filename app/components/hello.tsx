622e8dee114bf1cf01a05c06ac9f0ab9de121a9c
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-01-06 14:14:56 -0500

    docs: add some more comments about placeholder and hydration

--- app/components/hello.tsx
+++ app/components/hello.tsx
@@ -3,7 +3,7 @@
 // Appeared both in the server and the browser
 // Prerendered in the server, creating a static shell
 // Hydrated in the browser, meaning the JS is executed in the browser
-// Only the interactive parts are hydrated, meaning the non-interactive parts are still static on the server
+// Only the interactive parts are hydrated(before used are added as placeholders in the DOM), meaning the non-interactive parts are still static on the server
 export default function Hello() {
   console.log("Is this in the browser or server?");
   return <h1>Hello</h1>;
