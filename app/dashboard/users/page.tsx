d51d09877bbb7acc53b016bff89ed34c00b228df
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-01-08 09:04:42 -0500

    feat: create basic user dashboard page with a list of users

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const page = () => {
-  return <div>Users</div>;
+  return (
+    <div>
+      <h1>Dashboard Users</h1>
+      <ul className="mt-10">
+        <li>User 1</li>
+        <li>User 2</li>
+        <li>User 3</li>
+      </ul>
+    </div>
+  );
 };
 
 export default page;
